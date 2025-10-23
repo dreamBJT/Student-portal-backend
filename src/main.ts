ba6943d21ef37fa59da72ae79c7a0206f6dd92cb
dreambigbjt@gmail.com <dreambigbjt@gmail.com>
2025-10-23 16:01:26 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
       'http://localhost:3001',
       'http://localhost:4200',
       'http://127.0.0.1:3000',
-      'https://injibara-portal.vercel.app/',
+      'https://injibara-portal.vercel.app',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
